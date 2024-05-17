9d2494a866d9b09b6905ca63582ae9bb89512bb0
Justin Sijbolts <partydoos@gmail.com>
2024-05-17 14:23:26 +0200

    Login page frontend

--- pages/login.php
+++ pages/login.php
@@ -12,13 +12,13 @@
 <body>
     <div class="auth_container">
         <div class="login-form auth_form">
-            <div style="width:50%;" class="auth_form-left">
+            <div style="width:260px;" class="auth_form-left">
                 <img src="../assets/svg/logo/fiscariep-logo-white-rgb.svg" alt="Fiscariep Logo">
             </div>
             <div style="width:50%;padding:50px;" class="auth_form-right">
                 <h1>Login</h1>
 
-                <form action="login.php" method="post">
+                <div>
                     <div style="display:inline-grid;width:100%;">
                         <div class="auth_inputfield">
                             <label for="email">Email</label>
@@ -34,38 +34,47 @@
                     <button class="but_primary" type="submit"onclick="loginUser()">Login</button>
 
                     <p style="text-align:center;opacity:70%;margin-top:20px;">Don't have an account? <a href="#" onclick="toggleForm()">Register</a></p>
-                </form>
+                </div>
             </div>
         </div>
 
         <div class="register-form auth_form hidden">
-            <h1>Register</h1>
+            <div style="width:260px;" class="auth_form-left">
+                <img src="../assets/svg/logo/fiscariep-logo-white-rgb.svg" alt="Fiscariep Logo">
+            </div>
+            <div style="width:50%;padding:50px;" class="auth_form-right">
+                <h1>Register</h1>
 
-            <form action="register.php" method="post">
-                <div class="auth_inputfield">
-                    <label for="username">Username</label>
-                    <input type="text" id="register-username">
+                <div>
+                    <div style="display:inline-grid;width:100%;">
+                        <div class="auth_inputfield">
+                            <label for="username">Username</label>
+                            <input type="text" id="register-username">
+                        </div>
+
+                        <div class="auth_inputfield">
+                            <label for="email">Email</label>
+                            <input type="email" id="register-email">
+                        </div>
+
+                        <div style="display:flex;column-gap:10px">
+                            <div class="auth_inputfield">
+                                <label for="password">Password</label>
+                                <input type="password" id="register-password">
+                            </div>
+
+                            <div class="auth_inputfield">
+                                <label for="confirm_password">Confirm Password</label>
+                                <input type="password" id="register-password-confirm">
+                            </div>
+                        </div>
+                    </div>
+
+                    <p>Already have an account? <a href="#" onclick="toggleForm()">Login</a></p>
+
+                    <button style="width: 100%;" class="but_primary" type="submit" onclick="registerUser()">Register</button>
                 </div>
-
-                <div class="auth_inputfield">
-                    <label for="email">Email</label>
-                    <input type="email" id="register-email">
-                </div>
-
-                <div class="auth_inputfield">
-                    <label for="password">Password</label>
-                    <input type="password" id="register-password">
-                </div>
-
-                <div class="auth_inputfield">
-                    <label for="confirm_password">Confirm Password</label>
-                    <input type="password" id="register-password-confirm">
-                </div>
-
-                <p>Already have an account? <a href="#" onclick="toggleForm()">Login</a></p>
-
-                <button type="submit" onclick="registerUser()">Register</button>
-            </form>
+            </div>      
         </div>
         <span class="auth_copyright-text">Alle rechten voorbehouden.</span>
     </div>
