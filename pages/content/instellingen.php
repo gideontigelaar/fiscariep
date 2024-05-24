a4cd00085594afa0782f241fc7dda6d351ccf1c0
Gideon Tigelaar <gidtigelaar@gmail.com>
2024-05-24 15:47:33 +0200

    Implement password popup and add info to printjob file

--- pages/content/instellingen.php
+++ pages/content/instellingen.php
@@ -7,67 +7,46 @@ $userData = $stmt->fetch(PDO::FETCH_ASSOC);
 <script src="/assets/js/showError.js"></script>
 <div>
     <div>
-        <div>
-            <label for="username">Gebruikersnaam</label>
-            <input type="text" id="username" value="<?php echo $userData['username']; ?>">
-        </div>
-
-        <div>
-            <label for="email">E-mail</label>
-            <input type="email" id="email" value="<?php echo $userData['email']; ?>">
-        </div>
-
-        <div>
-            <label for="address">Adres</label>
-            <input type="text" id="address" value="<?php echo $userData['address']; ?>">
-        </div>
-
-        <div>
-            <label for="postal_code">Postcode</label>
-            <input type="text" id="postal_code" value="<?php echo $userData['postal_code']; ?>">
-        </div>
-
-        <div>
-            <label for="city">Plaats</label>
-            <input type="text" id="city" value="<?php echo $userData['city']; ?>">
-        </div>
-
-        <div>
-            <label for="province">Provincie</label>
-            <input type="text" id="province" value="<?php echo $userData['province']; ?>">
-        </div>
-
-        <div>
-            <label for="country">Land</label>
-            <input type="text" id="country" value="<?php echo $userData['country']; ?>">
-        </div>
-
-        <div>
-            <label for="phone_number">Telefoonnummer</label>
-            <input type="text" id="phone_number" value="<?php echo $userData['phone_number']; ?>">
-        </div>
-
-        <button class="but_primary" type="submit" onclick="updateSettings()">Opslaan</button>
+        <label for="username">Gebruikersnaam</label>
+        <input type="text" id="username" value="<?php echo $userData['username']; ?>">
     </div>
 
     <div>
-        <div>
-            <div>
-                <label for="oldPassword">Huidig wachtwoord</label>
-                <input type="password" id="oldPassword">
-            </div>
-
-            <div>
-                <label for="newPassword">Wachtwoord</label>
-                <input type="password" id="newPassword">
-            </div>
-
-            <div>
-                <label for="confirmPassword">Bevestig wachtwoord</span></label>
-                <input type="password" id="confirmPassword">
-            </div>
-        </div>
-
-        <button class="but_primary" type="submit" onclick="changePassword()">Opslaan</button>
+        <label for="email">E-mail</label>
+        <input type="email" id="email" value="<?php echo $userData['email']; ?>">
     </div>
+
+    <div>
+        <label for="address">Adres</label>
+        <input type="text" id="address" value="<?php echo $userData['address']; ?>">
+    </div>
+
+    <div>
+        <label for="postal_code">Postcode</label>
+        <input type="text" id="postal_code" value="<?php echo $userData['postal_code']; ?>">
+    </div>
+
+    <div>
+        <label for="city">Plaats</label>
+        <input type="text" id="city" value="<?php echo $userData['city']; ?>">
+    </div>
+
+    <div>
+        <label for="province">Provincie</label>
+        <input type="text" id="province" value="<?php echo $userData['province']; ?>">
+    </div>
+
+    <div>
+        <label for="country">Land</label>
+        <input type="text" id="country" value="<?php echo $userData['country']; ?>">
+    </div>
+
+    <div>
+        <label for="phone_number">Telefoonnummer</label>
+        <input type="text" id="phone_number" value="<?php echo $userData['phone_number']; ?>">
+    </div>
+
+    <button class="but_primary" type="submit" onclick="updateSettings()">Opslaan</button>
+
+    <button onclick="nextPopupStep('Wijzig wachtwoord', '','change-password')">Wijzig wachtwoord</button>
 </div>
---
+++ pages/popups/change-password.php
@@ -0,0 +1,20 @@
+<div>
+    <div>
+        <div>
+            <label for="oldPassword">Huidig wachtwoord</label>
+            <input type="password" id="oldPassword">
+        </div>
+
+        <div>
+            <label for="newPassword">Wachtwoord</label>
+            <input type="password" id="newPassword">
+        </div>
+
+        <div>
+            <label for="confirmPassword">Bevestig wachtwoord</span></label>
+            <input type="password" id="confirmPassword">
+        </div>
+    </div>
+
+    <button class="but_primary" type="submit" onclick="changePassword()">Opslaan</button>
+</div>
