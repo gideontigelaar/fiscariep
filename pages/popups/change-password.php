da1a8442aeff02d743ad60e55d3719f6167fb87c
Justin Sijbolts <partydoos@gmail.com>
2024-05-27 14:15:54 +0200

    Adjusts UI

--- pages/popups/change-password.php
+++ pages/popups/change-password.php
@@ -1,4 +1,4 @@
-<div>
+<div id="change-password-container">
     <div>
         <div class="gl_ordinary-input-field">
             <label for="oldPassword">Huidig wachtwoord</label>
@@ -16,9 +16,25 @@
         </div>
     </div>
 
-    <button class="but_primary" type="submit" onclick="changePassword()">Opslaan</button>
+    <button class="but_primary" id="save-button-change-pw" onclick="changePassword()">Opslaan</button>
 </div>
 
 <script>
-    console.log("change-password.php");
+    // if there are values that aren't filled in, make the but_primary disabled else enabled
+    document.getElementById('save-button-change-pw').classList.add('but_disabled');
+    // if ALL input fields inside of change-password-container are filled in, remove the but_disabled class from the save-button, else add it
+    document.getElementById('change-password-container').addEventListener('input', function() {
+        let inputs = document.querySelectorAll('#change-password-container input');
+        let filledIn = true;
+        inputs.forEach(input => {
+            if (input.value === '') {
+                filledIn = false;
+            }
+        });
+        if (filledIn) {
+            document.getElementById('save-button-change-pw').classList.remove('but_disabled');
+        } else {
+            document.getElementById('save-button-change-pw').classList.add('but_disabled');
+        }
+    });
 </script>
