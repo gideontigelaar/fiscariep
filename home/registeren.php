213956795f544f2cce0b857cbd079bd6dc243c24
Justin Sijbolts <partydoos@gmail.com>
2024-06-19 13:55:49 +0200

    Adds homepage and brings minor adjustments to several elements

---
+++ home/registeren.php
@@ -0,0 +1,8 @@
+<?php @include"barnav.php"; ?>
+
+    <h1>Registeren</h1>
+    
+
+    <p>Registeren</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fiscariep</title>
+<?php @include"home/navbar.php"; ?>
+            <div class="begroeting">
+            <?php
+            if(date('H') < '6') {
+            echo "<h3>Goedenacht, welkom bij Fiscariep!</h1>";
+            }
+            elseif (date('H') < '12') {
+            echo "<h3>Goedemorgen, welkom bij Fiscariep!</h1>";
+            }
+            elseif (date('H') < '18') {
+            echo "<h3>Goedemiddag, welkom bij Fiscariep!</h1>";
+            }
+            else {
+            echo "<h3>Goedeavond, welkom bij Fiscariep!</h1>";
+            }
+            ?>
+            </div>   
 
-    <link rel="stylesheet" href="css/import.css">
-</head>
-<body>
+            <div class="text1">
+                <h1>Welkom bij Fiscariep</h1>
+                <p>Op de website kan je alles vinden wat wij doen.<br> Hier kan je informatie vinden over de belastingdienst, belastingaangifte en nog veel meer.<br> Ook kan je hier terecht voor hulp bij je belastingaangifte en andere belastingzaken.<br> Kijk gerust rond op de website en als je vragen hebt, neem dan contact met ons op.</p>
+            </div>
 
-</body>
-</html>
+        </body>
+    </html>
